98bea43114bca922373d176c7a15d4d736380227
Nimish Shah <nimishshah2000@gmail.com>
2021-04-06 00:47:20 +0530

    add aadhar consts

---
+++ aadhar/const.js
@@ -0,0 +1,10 @@
+module.exports = {
+  version: 2.0,
+  publicData: {
+    ac: "public",
+    sa: "public",
+    ver: "2.0",
+    udc: "testAPI",
+    lk: "MBni88mRNM18dKdiVyDYCuddwXEQpl68dZAGBQ2nsOlGMzC9DkOVL5s",
+  },
+};

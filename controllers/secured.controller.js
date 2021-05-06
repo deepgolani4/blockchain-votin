a179212edb17e9f34d6a78e5f13e3f2c44738a7e
Nimish Shah <nimishshah2000@gmail.com>
2021-05-06 17:19:01 +0530

    AES Encryption added

--- controllers/secured.controller.js
+++ controllers/secured.controller.js
@@ -2,6 +2,8 @@ const luckyGen = require("../helpers/luckyNumGen");
 const pow = require("../helpers/modularExp");
 const Vote = require("../blockchain/blockchain").Vote;
 const client = require("../helpers/postgres");
+const generatePass = require("pr-pass");
+const crypto = require("crypto-js");
 
 module.exports = {
   vote: (req, res) => {
@@ -28,9 +30,21 @@ module.exports = {
      * You have lucky number as sharedKey from Diffie Hellman
      */
 
+    const secretAES = generatePass(req.headers.authorization, sharedKey);
+
     /**
      * Decrypt with the symmetric key generated from PR Pass.
      */
+
+    const cipher = crypto.AES.decrypt(
+      crypto.enc.Base64.parse(req.body.payload).toString(),
+      secretAES,
+      {
+        mode: crypto.mode.CTR,
+      }
+    ).toString();
+
+    req.body = JSON.parse(cipher);
 
     const { data, timestamp } = req.body;
     console.log(req.body);
